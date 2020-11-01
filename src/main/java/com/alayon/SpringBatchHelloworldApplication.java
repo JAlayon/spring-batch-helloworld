88e26ec241cc739a1f5f680c2c58b8fef34806b3
Jair Alayon <jair.atl.95@gmail.com>
2020-11-01 16:18:32 -0600

    Adding listeners, readers, etc to the app

--- src/main/java/com/alayon/SpringBatchHelloworldApplication.java
+++ src/main/java/com/alayon/SpringBatchHelloworldApplication.java
@@ -1,53 +1,25 @@
 package com.alayon;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.scope.context.ChunkContext;
-import org.springframework.batch.core.step.tasklet.Tasklet;
-import org.springframework.batch.repeat.RepeatStatus;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+import java.util.List;
 
-@EnableBatchProcessing
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class SpringBatchHelloworldApplication {
 
-	@Autowired
-	private JobBuilderFactory jobs;
-
-	@Autowired
-	private StepBuilderFactory steps;
-
-	public static void main(final String[] args) {
-		SpringApplication.run(SpringBatchHelloworldApplication.class, args);
-	}
-
-	@Bean
-	public Step step1() {
-		return steps.get("step-1").tasklet(helloworldTasklet()).build();
-	}
-
-	private Tasklet helloworldTasklet() {
-		return new Tasklet() {
-
-			@Override
-			public RepeatStatus execute(final StepContribution contribution, final ChunkContext chunkContext)
-					throws Exception {
-				System.out.println("Hello world");
-				return RepeatStatus.FINISHED;
-			}
-		};
-	}
-
-	@Bean
-	public Job helloWorldJob() {
-		return jobs.get("helloWorld").start(step1()).build();
+	public static void main(final String[] args) throws Exception {
+		// SpringApplication.run(SpringBatchHelloworldApplication.class, args);
+		int x = (x = 3) * 4;
+		System.out.println(x);
+		final List<StringBuilder> messages = List.of(new StringBuilder(), new StringBuilder());
+		messages.forEach(s -> s.append("helloworld"));
+		messages.forEach(s -> {
+			s.insert(5, ",");
+			System.out.println(s);
+		});
+		final Object t = new Integer(107);
+		final int k = (Integer) t.intValue() / 9;
+		System.out.println(k);
 	}
 
 }
